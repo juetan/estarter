7d97b2d7928ac0d2a04dddabe1dfa0255a9d7fda
juetan <contact@juetan.cn>
2023-03-09 21:00:49 +0800

    feat: 优化命令行输出样式

--- babel/core.js
+++ babel/core.js
@@ -12,12 +12,3 @@ const result = transformFileSync(path.join(__dirname, "./code/source.js"), {
 });
 
 console.log(result.code);
-
-/**
- * JS Parser 历史
- * esprima 基于SpiderMonkey的AST标准, 后来形成Estree标准 => acorn解析器(A tiny, fast JavaScript parser, written completely in JavaScript)
- * espree Eslint Fork自esprima
- * acorn Estree标准的实现
- *
- * 其他： terser typescript
- */
--- minimist/index.js
+++ minimist/index.js
@@ -1,22 +1,23 @@
 import cac from "cac";
-import prompts from "prompts";
-import ora from "ora";
-import { blue, green, bold } from "kolorist";
-import path from "path";
 import downloadGitRepo from "download-git-repo";
+import { blue, bold, green } from "kolorist";
+import ora from "ora";
+import path from "path";
+import prompts from "prompts";
 
-const LOGO = `
+const LOGO = blue(`
 ███████╗███████╗████████╗ █████╗ ██████╗ ████████╗███████╗██████╗
 ██╔════╝██╔════╝╚══██╔══╝██╔══██╗██╔══██╗╚══██╔══╝██╔════╝██╔══██╗
 █████╗  ███████╗   ██║   ███████║██████╔╝   ██║   █████╗  ██████╔╝
 ██╔══╝  ╚════██║   ██║   ██╔══██║██╔══██╗   ██║   ██╔══╝  ██╔══██╗
 ███████╗███████║   ██║   ██║  ██║██║  ██║   ██║   ███████╗██║  ██║
-╚══════╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝
-`
+╚══════╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝`);
 
-const getLOGO = ({ withVersion = true } = {}) => {
-  return blue(`${LOGO}${withVersion ? "\n版本: v0.0.1\n描述: 用于初始化项目的命令行工具\n" : ''}`);
+const getLOGO = ({ withVersion = true, emptyLine = true, descExtra = "" } = {}) => {
+  return `${LOGO}${withVersion ? `\n\n版本: v0.0.1\n描述: 用于初始化项目的命令行工具。${descExtra}${emptyLine ? '\n' : ''}` : ''}`;
 };
+
+const print = (...args) => console.log(...args);
 
 const download = async (repo, dest, options = {}) => {
   return new Promise((resolve, reject) => {
@@ -31,19 +32,20 @@ const app = cac("estarter");
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 app
-  .command("create [name]", "创建新项目")
-  .alias("init")
-  .option("-t, --template <template>", "指定模板类型, 可选值: vue、react、node")
-  .option("-f, --force", "强制移除已存在的目录")
+  .command("init [name]", "创建新项目")
+  .alias("create")
+  .option("-t, --template <template>", "指定模板类型, 可选值: vue、react、node, 默认值: vue")
+  .option("-f, --force", "强制移除已存在的目录, 默认值: false")
   .action(async (name, options) => {
-    console.log("命令：" + blue(bold(`创建新项目 \n`)));
+    print(getLOGO({ withVersion: false, emptyLine:true }));
+    print("\n命令: " + blue(bold(`创建新项目 \n`)));
 
     const opts = await prompts([
       {
         type: () => (name ? null : "text"),
         name: "name",
         message: "请输入你要创建的项目名称",
-        initial: "my-project",
+        initial: "app",
       },
       {
         type: () => (options.template ? null : "select"),
@@ -64,36 +66,30 @@ app
       },
     ]);
 
-    // console.log(`你输入的参数为：${opts.name}`);
+    const template = opts.template || options.template || "vue";
+    const targetDir = opts.targetDir || name || ".";
 
     const spinner = ora(bold("正在下载项目模板，请稍等...")).start();
 
-    // await download('juetan/templator', 'demo')
-
-    // download('juetan')
+    // await download('juetan/templator', path.join(".", targetDir));
+    await sleep(3000);
+    
     spinner.stop();
-    console.log(`${green("√")} ` + bold(`项目模板下载完成 \n`));
+    print(`${green("√")} ` + bold(`项目模板下载完成 \n`));
 
-    console.log(`${bold(green("创建成功!"))} 接下来你可以参照如下命令启动项目：`);
-    console.log("  cd ./demo");
-    console.log("  npm install");
-    console.log("  npm run dev");
-    console.log("");
+    print(`${bold(green("新项目创建完成!"))} 接下来你可以参照如下命令启动项目：`);
+    print("  cd ./demo");
+    print("  npm install");
+    print("  npm run dev\n");
   });
 
 app.command("generate <type>", "生成指定类型的模板");
 
-// console.log(`${LOGO}`);
-
-//   版本: v1.0.0
-//  用法: estarter [选项] [命令]
-//  帮助: estarter -h [命令]
-
 app.help((args) => {
   return args
     .map((arg) => {
-      if (arg.body.startsWith("estarter/")) {
-        return null;
+      if (arg.body.startsWith("estarter")) {
+        return { body: getLOGO({ emptyLine: false, withVersion: false }) }
       }
 
       if (arg.title === "Usage") {
@@ -105,7 +101,7 @@ app.help((args) => {
       }
 
       if (arg.title === "Commands") {
-        return { title: "命令", body: arg.body };
+        return { title: "命令", body: arg.body.replace(/\n\s*$/, "") };
       }
 
       if (arg.title?.startsWith("For more info")) {
@@ -123,17 +119,14 @@ app.help((args) => {
     .filter(Boolean);
 });
 
-// app.outputHelp("dd")
-
-// app.option("-v, --version", "显示版本号", { default: false }).
-
-app.version("0.0.1");
-
 app.usage("用于初始化项目的命令行工具");
 
-console.log(getLOGO());
+app.command("").option("-v, --version", "查看版本号").action((options) => {
+  if(options && options.version) {
+    print("v0.0.1");
+    return;
+  }
+  print(getLOGO({ descExtra: "你可以通过 estarter -h 查看帮助信息。" }));
+})
+
 app.parse();
-
-// const args = minimist(process.argv.slice(2), {})
-
-// console.log(args);
