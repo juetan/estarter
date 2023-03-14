d38f7de525e70232430a13963868b9ff668ec60a
luoer <luoer@szbeihai.com>
2023-03-14 09:48:28 +0800

    feat: 添加release-it

--- transformer/index.ts
+++ transformer/index.ts
@@ -1,13 +1,11 @@
-import "reflect-metadata";
-import { plainToInstance, instanceToPlain, Transform, Expose, Exclude, TransformFnParams } from "class-transformer";
+import 'reflect-metadata';
+import { plainToInstance, instanceToPlain, Transform, Expose, Exclude, TransformFnParams } from 'class-transformer';
 
 const toVolumeString = ({ value }: TransformFnParams) => `${value},${value}`;
-const toVolumeNumber = ({ value }: TransformFnParams) => Number(value.split(",")[0]);
+const toVolumeNumber = ({ value }: TransformFnParams) => Number(value.split(',')[0]);
 
 const toDataId = ({ value }: TransformFnParams) => value[0].id;
 const toData = ({ value }: TransformFnParams) => [{ id: value + 1001 }];
-
-const toTimeArray = ({ obj }: TransformFnParams) => [new Date(obj.startTime), new Date(obj.endTime)];
 
 class StartEndTime {
   @Expose()
@@ -34,7 +32,7 @@ class StartEndTime {
 
 class User extends StartEndTime {
   @Exclude()
-  @Expose({ name: "id" })
+  @Expose({ name: 'id' })
   guid: number;
 
   @Expose()
@@ -43,12 +41,10 @@ class User extends StartEndTime {
   volume: number;
 
   @Exclude()
-  @Expose({ name: "data" })
+  @Expose({ name: 'data' })
   @Transform(toDataId, { toClassOnly: true })
   @Transform(toData, { toPlainOnly: true })
   dataId: number;
-
-  [key: string]: any;
 }
 
 interface SUser {
@@ -58,11 +54,11 @@ interface SUser {
   [key: string]: any;
 }
 
-const p: SUser = { id: 1, volume: "90,90", data: [{ id: 10 }], startTime: '00:00:01', endTime: '00:00:02' };
+const p: SUser = { id: 1, volume: '90,90', data: [{ id: 10 }], startTime: '00:00:01', endTime: '00:00:02' };
 
 const user = plainToInstance(User, p, { excludeExtraneousValues: true });
 const user1 = instanceToPlain(user);
 
-console.log('\n Plain Object', p, '\n Instance Object',user, '\n Plain Object', user1);
+console.log('\n Plain Object: ', p, '\n Instance Object: ', user, '\n Plain Object: ', user1);
 
 // npx tsx watch index.ts
